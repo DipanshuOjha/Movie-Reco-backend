7404560d75b10a3f4ef2992e8a902a577fe07672
DipanshuOjha <dipanshuojha12@gmail.com>
2025-06-28 09:15:41 +0530

    added database status in health checkpoint

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const authroute = require('./route/auth')
 const movieroute = require('./route/movies')
 
+const { PrismaClient }=  require('../backend/generated/prisma/client');
+const prisma = new PrismaClient()
+
 const starttime = Date.now();
 
 
@@ -14,17 +17,29 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
     const currtime = Date.now();
     const uptimeMillis = currtime-starttime;
 
     const seconds = Math.floor(uptimeMillis / 1000) % 60;
     const minutes = Math.floor(uptimeMillis / (1000 * 60)) % 60;
     const hours = Math.floor(uptimeMillis / (1000 * 60 * 60));
-    res.json({
-        status:"Healthy",
-        uptime: `${hours}h ${minutes}m ${seconds}s`,
+    try {
+    await prisma.$queryRaw`SELECT 1`
+    
+    res.status(200).json({
+      status: 'healthy',
+      database: 'connected',
+      uptime: `${hours}h ${minutes}m ${seconds}s`
     })
+  } catch (error) {
+    console.error('Database connection error:', error);
+    res.status(503).json({
+      status: 'unhealthy',
+      database: 'disconnected',
+      uptime: `${hours}h ${minutes}m ${seconds}s`
+    })
+  }
 })
 
 app.use('/api/auth',authroute)
